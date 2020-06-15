bb6c3ff5b3cda6dffd0059055cbb804eee56e689
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-06-15 13:50:23 -0500

    spacing for h1 and search bar and list

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+
+
+const useStyles = makeStyles({
+    center: {
+       textAlign: 'center',
+    },
+    marginBottum: {
+        margin: '0 0 1rem 0',
+    },
+    container: {
+        height: '100vh'
+    }
+});
 
 function Home() {
+    const classes = useStyles();
 
     function Form7DayRow(){
         return (
@@ -56,34 +83,61 @@ function Home() {
     }
 
     return (
-        <Grid container>
-            <Grid item xs={12}>
-                <h1>RESTful Weather</h1>
+        <Container className={classes.container} maxWidth='md'>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <h1 className={classes.center}>RESTful Weather</h1>
+                    </Grid>
+                <Grid item l={5}>
+                    <form>
+                        <TextField 
+                            className={classes.marginBottum}
+                            id="standard-basic" 
+                            label="Search" 
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end"><Button><SearchIcon /></Button></InputAdornment>,
+                            }} 
+                        />
+                    </form>
+                    <Card>
+                        <CardContent>
+                            <List>
+                                <ListItem button>
+                                    <ListItemText primary="Los Angeles, CA" />
+                                </ListItem>
+                                <Divider />
+                                <ListItem button>
+                                    <ListItemText primary="New York, NY" />
+                                </ListItem>
+                                <Divider />
+                                <ListItem button>
+                                    <ListItemText primary="Chicago, IL" />
+                                </ListItem>
+                                <Divider />
+                                <ListItem button>
+                                    <ListItemText primary="London, England" />
+                                </ListItem>
+                                <Divider />
+                                <ListItem button>
+                                    <ListItemText primary="Toykyo, Japan" />
+                                </ListItem>
+                            </List>
+                        </CardContent>
+                    </Card>
                 </Grid>
-            <Grid item l={5}>
-                <form>
-                    <TextField id="standard-basic" label="Search" />
-                </form>
-                <Paper>
-                    Los Angeles, CA
-                    New York, NY
-                    Chicago, IL
-                    London, England
-                    Toykyo, Japan
-                </Paper>
+                <Grid item l={7}>
+                    <Paper>Current Weather</Paper>
+                    <h3>Hourly Forcast:</h3>
+                    <Grid container item xs={12}>
+                        <FormHourlyRow />
+                    </Grid>
+                    <h3>7-Day Forcast:</h3>
+                    <Grid container item xs={12}>
+                        <Form7DayRow />
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid item l={7}>
-                <Paper>Current Weather</Paper>
-                <h3>Hourly Forcast:</h3>
-                <Grid container item xs={12}>
-                    <FormHourlyRow />
-                </Grid>
-                <h3>7-Day Forcast:</h3>
-                <Grid container item xs={12}>
-                    <Form7DayRow />
-                </Grid>
-            </Grid>
-        </Grid>
+        </Container>
     )
 }
 
